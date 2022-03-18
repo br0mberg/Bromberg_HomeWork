a6ce4f9cf4257ec1867c03191686b31e5082426d
Andrey Brombin <arcadka@gmail.com>
2022-03-18 12:52:10 +0300

    save

--- project/include/utils.h
+++ project/include/utils.h
@@ -1,9 +1,8 @@
-#define UTILS_H
 #ifndef UTILS_H
-
+#define UTILS_H
 #define filename "transaction.dat";
 
 // This comment is very useful
 printf("This message is very useful too")
 
-#endif //UTILS_H
+#endif // UTILS_H
