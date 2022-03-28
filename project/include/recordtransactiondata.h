88abbb0014e26853d23f7b64fd7d323b6383a4d5
Andrey Brombin <arcadka@gmail.com>
2022-03-29 01:51:23 +0300

    ?

--- project/include/recordtransactiondata.h
+++ project/include/recordtransactiondata.h
@@ -1,8 +1,6 @@
 #ifndef PROJECT_INCLUDE_RECORDTRANSACTIONDATA_H_
 #define PROJECT_INCLUDE_RECORDTRANSACTIONDATA_H_
 
-#include "../include/structs.h"
-
 void recordtransactiondata(const char* fname);
 
 #endif  // PROJECT_INCLUDE_RECORDTRANSACTIONDATA_H_
