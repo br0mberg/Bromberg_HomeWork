5cc1a08b08cf22d5d22e52c49469fa4754664916
Andrey Brombin <arcadka@gmail.com>
2022-03-29 01:47:39 +0300

    multiDataDef

--- project/include/recordclientdata.h
+++ project/include/recordclientdata.h
@@ -1,7 +1,6 @@
 #ifndef PROJECT_INCLUDE_RECORDCLIENTDATA_H_
 #define PROJECT_INCLUDE_RECORDCLIENTDATA_H_
 
-#include "../include/structs.h"
 #include "../include/out_inputdata.h"
 
 void recordclientdata(const char* fname);
--- project/include/updclientdata.h
+++ project/include/updclientdata.h
@@ -1,7 +1,6 @@
 #ifndef PROJECT_INCLUDE_UPDCLIENTDATA_H_
 #define PROJECT_INCLUDE_UPDCLIENTDATA_H_
 
-#include "../include/structs.h"
 #include "../include/out_inputdata.h"
 
 void updclientdata(const char* fname_record, const char* fname_transaction, const char* fname_blackrecord);
