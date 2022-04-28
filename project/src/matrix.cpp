006ff633345b1c6f54b6030bac585fdcc137f3cd
Andrey Brombin <arcadka@gmail.com>
2022-04-28 16:33:07 +0300

    succes

--- project/src/matrix.cpp
+++ project/src/matrix.cpp
@@ -52,7 +52,7 @@ double& Matrix::operator()(size_t i, size_t j) {
 bool Matrix::operator==(const Matrix& rhs) const {
     for (size_t i = 0; i < rows; ++i) {
         for (size_t j = 0; j < cols; ++j) {
-            if (std::abs(values[i*cols + j] - rhs(i, j)) > std::numeric_limits<double>::epsilon()) {
+            if (std::abs(values[i*cols + j] - rhs(i, j)) > std::numeric_limits<float>::epsilon()) {
                 return false;
             }
         }
@@ -159,13 +159,17 @@ Matrix Matrix::transp() const {
 
 double Matrix::det() const {
     double val = 0;
+    if ((*this).getCols() != rows || (*this).getRows() != cols) {
+        throw DimensionMismatch((*this));
+    }
+
     if (this->rows == 1) {
-        val = this->values.at(0);
+        val = this->values[0];
         return val;
     }
 
     if (this->rows == 2) {
-        val += this->values.at(0) * this->values.at(3) - this->values.at(2) * this->values.at(1);
+        val += this->values[0] * this->values[3] - this->values[2] * this->values[1];
         return val;
     }
 
@@ -175,7 +179,7 @@ double Matrix::det() const {
         for (size_t j = 1, next_j = 0; j < this->rows; ++j, ++next_j) {
             for (size_t k = 0, next_k = 0; k < this->cols; ++k, ++next_k) {
                 if (k != i) {
-                    matrix_norder(next_j, next_k) = this->values.at(j * this->cols + k);
+                    matrix_norder(next_j, next_k) = this->values[j * this->cols + k];
                 } else {
                     --next_k;
                 }
@@ -183,29 +187,30 @@ double Matrix::det() const {
         }
         double res = 0;
         res = matrix_norder.det();
-        val += pow(-1, ratio) * values.at(i) * res;
+        val += pow(-1, ratio) * values[i] * res;
     }
     return val;
 }
 
 Matrix Matrix::adj() const {
     double deter = 0;
-    deter = det();
+    deter = (*this).det();
+    if (rows != cols) {
+        throw DimensionMismatch((*this));
+    }
 
     if (!deter) {
         fprintf(stderr, "ERROR_DETERMINANT");
         throw false;
     }
 
-    Matrix adj_matrix(rows, cols);
+    Matrix adj_matrix = Matrix(rows, cols);
 
     if (rows == 1) {
         adj_matrix(rows - 1, cols - 1) = 1;
         return adj_matrix;
     }
-
-    Matrix transp_matrix = transp();
-
+    Matrix transp_matrix = (*this).transp();
     for (size_t i = 0; i < rows; ++i) {
         for (size_t j = 0; j < cols; ++j) {
             Matrix temp_matrix(rows - 1, cols - 1);
@@ -233,24 +238,27 @@ Matrix Matrix::adj() const {
 }
 
 Matrix Matrix::inv() const {
-    double determinant = 0;
-    determinant = det();
-    if (!determinant) {
-        fprintf(stderr, "ERROR_DETERMINANT");
-        throw false;
+    double deter_matrix = (*this).det();
+    if (!deter_matrix) {
+        throw SingularMatrix();
     }
 
-    Matrix inv_matrix = adj();
-
+    if (rows != cols) {
+        throw DimensionMismatch(*this);
+    }
+    Matrix inv_matrix = Matrix(rows, cols);
     for (size_t i = 0; i < rows; ++i) {
         for (size_t j = 0; j < cols; ++j) {
-            double temp = 0;
-            temp = inv_matrix(i, j);
-            temp *= 1/determinant;
-            inv_matrix(i, j) = temp;
+            inv_matrix(i, j) = values[i * inv_matrix.getCols() + j];
         }
     }
-    throw SingularMatrix();
-    return inv_matrix;
+    Matrix adj_matrix = inv_matrix.adj();
+    double determinate = 0;
+    determinate = inv_matrix.det();
+    if (determinate == 0) {
+        throw SingularMatrix();
+    }
+    Matrix res = adj_matrix.operator*(1. / determinate);
+    return res;
 }
 }  // namespace prep
